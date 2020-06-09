cb617d2ec43bf000554a5c00f17351984316f8ee
ODA-Fiori <chelsea@montevideo.com.uy>
2020-06-09 15:58:39 +0000

    MyFirstCommit

---
+++ MyFirstProject/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.logali_namespace.MyFirstProject.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
